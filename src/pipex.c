acbe5a9acf2968b19326f610542dec7dd5ee84bc
fritz-gabler <58051701+fritz-gabler@users.noreply.github.com>
2025-12-09 10:04:59 +0100

    add input check in pipex main

--- src/pipex.c
+++ src/pipex.c
@@ -1,14 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fritzgabler <marvin@42.fr>                 +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/09 09:40:18 by fritzgabler       #+#    #+#             */
+/*   Updated: 2025/12/09 10:03:44 by fritzgabler      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "pipex.h"
 
-int main(int argument_count, char **arguemt_vector, char **enviroment_ptr)
+int	main(int argument_count, char **arguemt_vector, char **enviroment_ptr)
 {
 	(void) arguemt_vector;
 	(void) enviroment_ptr;
-
-	if (is_input_valid(argument_count) == false)
+	if (is_input_valid(argument_count, arguemt_vector) == false)
 	{
 		write(STDERR_FILENO,
-		"Invalid input, correct usage: ./pipex file1 cmd1 cmd2 file2\n", 30);
+			"Invalid input, correct usage: ./pipex file1 cmd1 cmd2 file2\n", 61);
 		return (1);
 	}
 	return (0);
