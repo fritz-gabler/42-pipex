4873a9dc4474de090d91fd4a7a686d0f24dbee12
fritz-gabler <58051701+fritz-gabler@users.noreply.github.com>
2025-12-14 10:37:07 +0100

    add setup struct function

--- src/setup_struct/setup_struct.c
+++ src/setup_struct/setup_struct.c
@@ -6,7 +6,7 @@
 /*   By: fritzgabler <marvin@42.fr>                 +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/09 10:29:30 by fritzgabler       #+#    #+#             */
-/*   Updated: 2025/12/09 13:19:43 by fritzgabler      ###   ########.fr       */
+/*   Updated: 2025/12/14 10:01:59 by fgabler          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,28 +14,20 @@
 #include <stdio.h>
 #include <string.h>
 
-static int get_file_fd(char *infile_str, int flags);
-
 void setup_struct(t_data *data, int argument_count, char **arguemt_vector,
                   char **enviroment_ptr)
 {
 	char *out_file_str;
 
+	out_file_str = arguemt_vector[argument_count - 1];
 	data->arguemt_vector = arguemt_vector;
 	data->arguemtn_count = argument_count;
 	data->enviroment_ptr = enviroment_ptr;
 	data->input_file_fd = get_file_fd(arguemt_vector[1], O_RDONLY);
-	out_file_str = arguemt_vector[argument_count - 1];
 	data->output_file_fd = get_file_fd(out_file_str, O_WRONLY | O_CREAT);
 	data->path = get_all_paths(enviroment_ptr);
-}
-
-static int get_file_fd(char *infile_str, int flags)
-{
-	int file_fd;
-
-	file_fd = open(infile_str, flags);
-	if (file_fd == -1)
-		exit(0);
-	return (file_fd);
+	data->number_of_commands = argument_count - 3;
+	data->num_of_current_command = 0;
+	data->commands = get_commands(data->number_of_commands, arguemt_vector);
+	set_pipe_fds(data);
 }
