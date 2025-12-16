3a73b8d447d7b0a6d2924e7b1898f79564659f35
fritz-gabler <58051701+fritz-gabler@users.noreply.github.com>
2025-12-16 10:53:46 +0100

    put dup2 args in correct order

--- src/command_handeling/replace_stdin_save.c
+++ src/command_handeling/replace_stdin_save.c
@@ -4,7 +4,7 @@ void replace_stdin_save(int file_fd_to_replace_with)
 {
 	int return_of_dup2;
 
-	return_of_dup2 = dup2(STDIN_FILENO, file_fd_to_replace_with);
+	return_of_dup2 = dup2(file_fd_to_replace_with, STDIN_FILENO);
 	if(return_of_dup2 == -1)
 		//throw Error
 		return;
--- src/command_handeling/replace_stdout_save.c
+++ src/command_handeling/replace_stdout_save.c
@@ -4,7 +4,7 @@ void replace_stdout_save(int file_fd_to_replace_with)
 {
 	int return_of_dup2;
 
-	return_of_dup2 = dup2(STDIN_FILENO, file_fd_to_replace_with); // check if input_file_fd really gets closed
+	return_of_dup2 = dup2(file_fd_to_replace_with, STDOUT_FILENO); // check if input_file_fd really gets closed
 	if(return_of_dup2 == -1)
 		//throw Error
 		return;
