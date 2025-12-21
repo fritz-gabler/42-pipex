b4e7a875ef6a7176b6187350dfd5f18193589064
fritz-gabler <58051701+fritz-gabler@users.noreply.github.com>
2025-12-21 13:33:50 +0100

    add test if just splitting cmd works

--- src/command_handeling/resolve_path.c
+++ src/command_handeling/resolve_path.c
@@ -3,7 +3,7 @@
 #include <stdio.h>
 
 static char *get_current_command(const t_data *data);
-static char *get_path_to_executable(const t_data *data);
+static char *get_path_to_executable(const t_data *data, int current_path_pos);
 
 void resolve_path(t_data *data)
 {
@@ -13,7 +13,7 @@ void resolve_path(t_data *data)
 	i = 0;
 	while (data->path[i] != NULL)
 	{
-		path_to_executable = get_path_to_executable(data);
+		path_to_executable = get_path_to_executable(data, i);
 		if (access(path_to_executable, X_OK) == IS_CORRECT_PATH)
 		{
 			data->path_to_executable = path_to_executable;
@@ -24,14 +24,15 @@ void resolve_path(t_data *data)
 	}
 }
 
-static char *get_path_to_executable(const t_data *data)
+static char *get_path_to_executable(const t_data *data, int current_path_pos)
 {
 	char *current_command;
 	char *path_to_executable;
 
 	current_command = get_current_command(data);
-	path_to_executable = ft_strjoin(data->path[i], "/"); // check for not freed mem
+	path_to_executable = ft_strjoin(data->path[current_path_pos], "/"); // check for not freed mem
 	path_to_executable = ft_strjoin(path_to_executable, current_command); // als here
+	return (path_to_executable);
 }
 
 static char *get_current_command(const t_data *data)
