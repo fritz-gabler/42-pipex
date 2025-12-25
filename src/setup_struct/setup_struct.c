329ef2cb9bb34441cf70308a0bed10b672a27c01
fritz-gabler <58051701+fritz-gabler@users.noreply.github.com>
2025-12-25 10:05:25 +0100

    change setup struct structure fitting new requirements

--- src/setup_struct/setup_struct.c
+++ src/setup_struct/setup_struct.c
@@ -6,28 +6,43 @@
 /*   By: fritzgabler <marvin@42.fr>                 +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/09 10:29:30 by fritzgabler       #+#    #+#             */
-/*   Updated: 2025/12/22 08:01:26 by fgabler          ###   ########.fr       */
+/*   Updated: 2025/12/25 09:58:25 by fgabler          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
-#include <stdio.h>
-#include <string.h>
+#include "libft.h"
 
-void setup_struct(t_data *data, int argument_count, char **arguemt_vector,
-                  char **enviroment_ptr)
+static void setup_first_child(t_data *data, char **argv, char **envp);
+static void fail_check(t_data *data, t_child *child);
+
+void setup_struct(t_data *data, int argc, char **argv, char **envp)
 {
-	char *out_file_str;
-
-	out_file_str = arguemt_vector[argument_count - 1];
-	data->arguemt_vector = arguemt_vector;
-	data->arguemtn_count = argument_count;
-	data->enviroment_ptr = enviroment_ptr;
-	data->input_file_fd = get_infile_fd(arguemt_vector[1]);
-	data->output_file_fd = get_outfile_fd(out_file_str);
-	data->path = get_all_paths(enviroment_ptr);
-	data->number_of_commands = argument_count - 3;
-	data->num_of_current_command = 0;
-	data->commands = get_commands(data->number_of_commands, arguemt_vector);
+	(void) argc;
+	initialize_data(data);
 	set_pipe_fds(data);
+	setup_first_child(data, argv, envp);
+}
+
+static void setup_first_child(t_data *data, char **argv, char **envp)
+{
+	t_child *first_child;
+
+	first_child = &data->first_child;
+	first_child->execve_argv = ft_split(argv[2], ' ');
+	first_child->path = get_resolved_path(first_child->execve_argv[0], envp);
+	first_child->input_file_fd = open(argv[1], O_RDONLY);
+	fail_check(data, &data->first_child);
+}
+
+static void fail_check(t_data *data, t_child *child)
+{
+	if (data->pipe_fds[0] == -1 || data->pipe_fds[1] == -1)
+		exit_clean(data, __FILE__, __LINE__);
+	else if (child->output_file_fd == -1 && child->input_file_fd == -1)
+		exit_clean(data, __FILE__, __LINE__);
+	else if (child->execve_argv == NULL)
+		exit_clean(data, __FILE__, __LINE__);
+	else if (child->path == NULL)
+		exit_clean(data, __FILE__, __LINE__);
 }
