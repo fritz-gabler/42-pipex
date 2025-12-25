23bd7b2d21e68e73ddfdb04d1cae43b12bfbeee2
fritz-gabler <58051701+fritz-gabler@users.noreply.github.com>
2025-12-25 11:32:50 +0100

    uncommend set std out

--- src/child_handeling/first_child.c
+++ src/child_handeling/first_child.c
@@ -1,8 +1,7 @@
-#include "libft.h"
 #include "pipex.h"
 
 static void set_stdin_of_first_child(t_data *data);
-//static void set_stdout_of_first_child(t_data *data);
+static void set_stdout_of_first_child(t_data *data);
 
 void first_child(t_data *data)
 {
@@ -10,7 +9,7 @@ void first_child(t_data *data)
 
 	first_child = &data->first_child;
 	set_stdin_of_first_child(data);
-	//set_stdout_of_first_child(data);
+	set_stdout_of_first_child(data);
 	execve(first_child->path, first_child->execve_argv, data->envp);
 }
 
@@ -21,7 +20,6 @@ static void set_stdin_of_first_child(t_data *data)
 	close(data->first_child.input_file_fd);
 }
 
-/*
 static void set_stdout_of_first_child(t_data *data)
 {
 	if (dup2(data->pipe_fds[WRITE], STDOUT_FILENO) == -1)
@@ -29,4 +27,3 @@ static void set_stdout_of_first_child(t_data *data)
 	close(data->pipe_fds[WRITE]);
 	close(data->pipe_fds[READ]);
 }
-*/
