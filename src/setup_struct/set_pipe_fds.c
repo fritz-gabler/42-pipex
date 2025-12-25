96e9d25c96fdeac6b2e8d4d9235f70c15a6f7773
fritz-gabler <58051701+fritz-gabler@users.noreply.github.com>
2025-12-25 10:04:57 +0100

    just return in set pipe and error check later

--- src/setup_struct/set_pipe_fds.c
+++ src/setup_struct/set_pipe_fds.c
@@ -5,7 +5,7 @@ void	set_pipe_fds(t_data *data)
 	int pipe_fds[2];
 
 	if (pipe(pipe_fds) == -1)
-		exit(0); // CLOSE AND FREE EVERYTHING
+		return	;
 	data->pipe_fds[WRITE] = pipe_fds[WRITE];
 	data->pipe_fds[READ] = pipe_fds[READ];
 }
